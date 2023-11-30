7d5ba6b84368bf5d5d6db354ad607dc5acd803d2
HackerADbro <150064036+HackerADbro@users.noreply.github.com>
2023-11-30 07:53:19 +0530

    Update details.py

--- details.py
+++ details.py
@@ -1,7 +1,7 @@
 api_id = 20543083
 api_hash = "8c0491f39646893e947098b6133c44cb"
-bot_token = "6454742390:AAEo86rRMuxd5VV3Yr4vNwKPHYLVbggbLVM"
-auth_users = [6123937721]
-sudo_user = 6123937721
-log_channel = -1001903653148
-txt_channel = -1001863602838
+bot_token = "6507030900:AAGDgQ27SfvPrO_yA1TU5_KtuB7WRF1x8Wk"
+auth_users = [6693536228]
+sudo_user = 6693536228
+log_channel = -4057097258
+txt_channel = -4050496587
